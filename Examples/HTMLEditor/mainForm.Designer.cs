5b4f57e31da7da7b3193d6a5cd644e136dbe592d
DCSItoDIGA <guido.agnesmeyer@itagnesmeyer.de>
2022-01-18 16:57:51 +0100

    HTML-Editor

---
+++ Examples/HTMLEditor/mainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace HTMLEditor
+{
+    partial class mainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webViewCode = new Diga.WebView2.WinForms.WebView();
+            this.webViewPreView = new Diga.WebView2.WinForms.WebView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(877, 520);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webViewCode);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webViewPreView);
+            this.splitContainer1.Size = new System.Drawing.Size(877, 520);
+            this.splitContainer1.SplitterDistance = 292;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // webViewCode
+            // 
+            this.webViewCode.AreBrowserAcceleratorKeysEnabled = true;
+            this.webViewCode.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.webViewCode.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.webViewCode.DefaultContextMenusEnabled = false;
+            this.webViewCode.DefaultScriptDialogsEnabled = true;
+            this.webViewCode.DevToolsEnabled = true;
+            this.webViewCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webViewCode.EnableMonitoring = false;
+            this.webViewCode.HtmlContent = null;
+            this.webViewCode.IsGeneralAutoFillEnabled = false;
+            this.webViewCode.IsPasswordAutosaveEnabled = false;
+            this.webViewCode.IsScriptEnabled = true;
+            this.webViewCode.IsStatusBarEnabled = false;
+            this.webViewCode.IsWebMessageEnabled = true;
+            this.webViewCode.IsZoomControlEnabled = true;
+            this.webViewCode.Location = new System.Drawing.Point(0, 0);
+            this.webViewCode.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.webViewCode.MonitoringFolder = null;
+            this.webViewCode.MonitoringUrl = null;
+            this.webViewCode.Name = "webViewCode";
+            this.webViewCode.RemoteObjectsAllowed = true;
+            this.webViewCode.Size = new System.Drawing.Size(292, 520);
+            this.webViewCode.TabIndex = 0;
+            this.webViewCode.Url = "";
+            this.webViewCode.ZoomFactor = 0D;
+            this.webViewCode.NavigationCompleted += new System.EventHandler<Diga.WebView2.Wrapper.EventArguments.NavigationCompletedEventArgs>(this.webViewCode_NavigationCompleted);
+            this.webViewCode.WebViewCreated += new System.EventHandler(this.webViewCode_WebViewCreated);
+            // 
+            // webViewPreView
+            // 
+            this.webViewPreView.AreBrowserAcceleratorKeysEnabled = true;
+            this.webViewPreView.BackColor = System.Drawing.Color.White;
+            this.webViewPreView.DefaultBackgroundColor = System.Drawing.Color.Empty;
+            this.webViewPreView.DefaultContextMenusEnabled = true;
+            this.webViewPreView.DefaultScriptDialogsEnabled = true;
+            this.webViewPreView.DevToolsEnabled = true;
+            this.webViewPreView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webViewPreView.EnableMonitoring = false;
+            this.webViewPreView.HtmlContent = null;
+            this.webViewPreView.IsGeneralAutoFillEnabled = false;
+            this.webViewPreView.IsPasswordAutosaveEnabled = false;
+            this.webViewPreView.IsScriptEnabled = true;
+            this.webViewPreView.IsStatusBarEnabled = false;
+            this.webViewPreView.IsWebMessageEnabled = true;
+            this.webViewPreView.IsZoomControlEnabled = true;
+            this.webViewPreView.Location = new System.Drawing.Point(0, 0);
+            this.webViewPreView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.webViewPreView.MonitoringFolder = null;
+            this.webViewPreView.MonitoringUrl = null;
+            this.webViewPreView.Name = "webViewPreView";
+            this.webViewPreView.RemoteObjectsAllowed = true;
+            this.webViewPreView.Size = new System.Drawing.Size(581, 520);
+            this.webViewPreView.TabIndex = 0;
+            this.webViewPreView.Url = null;
+            this.webViewPreView.ZoomFactor = 0D;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(877, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(97, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectAllToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 70);
+            // 
+            // selectAllToolStripMenuItem
+            // 
+            this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.selectAllToolStripMenuItem.Text = "Select All";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 544);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(877, 18);
+            this.panel2.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 562);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "Code-Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Diga.WebView2.WinForms.WebView webViewCode;
+        private Diga.WebView2.WinForms.WebView webViewPreView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ Examples/HTMLEditor/mainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Diga.WebView2.WinForms.Scripting.DOM;
+
+namespace HTMLEditor
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void webViewCode_WebViewCreated(object sender, EventArgs e)
+        {
+            string editHTMLPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            editHTMLPath = Path.Combine(editHTMLPath, "simple.html");
+            Uri uri = new Uri(editHTMLPath);
+            this.webViewCode.Navigate(uri.AbsolutePath);
+        }
+
+        private string CleanupDOMString(string html)
+        {
+            if (html.StartsWith("\""))
+                html = html.Substring(1);
+            if (html.EndsWith("\""))
+            {
+                html = html.Substring(0, html.Length - 1);
+            }
+
+            html = System.Text.RegularExpressions.Regex.Unescape(html);
+            //string enc = html= System.Text.RegularExpressions.Regex.Escape(html);
+            html = html.Replace("\n", Environment.NewLine);
+            return html;
+        }
+        private async void webViewCode_NavigationCompleted(object sender, Diga.WebView2.Wrapper.EventArguments.NavigationCompletedEventArgs e)
+        {
+            DOMDocument doc = this.webViewCode.GetDOMDocument();
+            DOMElement button = await doc.getElementById("bnTest");
+            if (button == null)
+            {
+                await this.webViewCode.GetDOMWindow().alert("Could not find bnTest");
+                return;
+            }
+
+            DOMEventListenerScript script = new DOMEventListenerScript(button);
+            await button.addEventListener("click", script, false);
+            button.DomEvent += async (eo,ev) =>
+            {
+                if (ev.EventName == "click")
+                {
+                    string value =await this.webViewCode.ExecuteScriptAsync("return editor.getValue();");
+                    value = CleanupDOMString(value);
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        this.webViewPreView.NavigateToString(value);
+                    }
+                }
+            };
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
